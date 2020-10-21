f90e505ae3735d21d48ea873cd13cf22e2335817
Dale Myszewski II <dale.myszewski@gmail.com>
2020-10-21 10:25:20 -0500

    Changes

--- iCache.Client/CacheClient.cs
+++ iCache.Client/CacheClient.cs
@@ -1,8 +1,70 @@
 ﻿using System;
+using System.Text;
 
 namespace iCache.Client
 {
-    public class CacheClient
+    public class CacheClient : IDisposable
     {
+        private bool disposedValue;
+        private string _username;
+        private string _password;
+        private string _authHeaderValue;
+
+        public CacheClient(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username))
+                throw new ArgumentNullException("username", "You must supply the username parameter!");
+
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException("password", "You must supply the password parameter!");
+
+            _username = username;
+            _password = password;
+            _authHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{_username}:{_password}"));
+        }
+
+        #region Keys
+
+        #endregion
+
+        #region Queues
+
+        #endregion
+
+        #region Dispose Method
+        protected virtual void Dispose(bool disposing)
+        {
+            _username = null;
+            _password = null;
+            _authHeaderValue = null;
+
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~CacheClient()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        void IDisposable.Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
     }
 }
---
+++ iCache.Tests/Middleware/BasicAuthenticationHandlerTests.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using iCache.API.Handlers;
+using iCache.API.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Net.Http.Headers;
+using Moq;
+using Xunit;
+using iCache.API.Services;
+using iCache.Common.Models;
+using iCache.API;
+using System.ComponentModel;
+
+namespace iCache.Tests
+{
+    public class BasicAuthenticationHandlerTests
+    {
+        private readonly Mock<IOptionsMonitor<AuthenticationSchemeOptions>> _options;
+        private readonly Mock<ILoggerFactory> _loggerFactory;
+        private readonly Mock<UrlEncoder> _encoder;
+        private readonly Mock<ISystemClock> _clock;
+        private readonly Mock<UserService> _userService;
+        private readonly BasicAuthenticationHandler _handler;
+
+        public BasicAuthenticationHandlerTests()
+        {
+            _options = new Mock<IOptionsMonitor<AuthenticationSchemeOptions>>();
+            _options.Setup(x => x.Get("Basic")).Returns(new AuthenticationSchemeOptions());
+
+            var logger = new Mock<ILogger<BasicAuthenticationHandler>>();
+            _loggerFactory = new Mock<ILoggerFactory>();
+            _loggerFactory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(logger.Object);
+
+            _encoder = new Mock<UrlEncoder>();
+            _clock = new Mock<ISystemClock>();
+
+            _userService = new Mock<UserService>();
+
+            _handler = new BasicAuthenticationHandler(_options.Object, _loggerFactory.Object, _encoder.Object, _clock.Object, _userService.Object);
+        }
+
+        private string EncodeAuthData(string username, string password)
+        {
+            string preencoded = $"{username}:{password}";
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(preencoded));
+        }
+
+        [Fact]
+        public async Task NotAuthorizedNoHeader()
+        {
+            var context = new DefaultHttpContext();
+
+            await _handler.InitializeAsync(
+                new AuthenticationScheme("Basic",
+                "Basic",
+                typeof(BasicAuthenticationHandler)),
+                context);
+
+            var result = await _handler.AuthenticateAsync();
+
+            Assert.False(result.Succeeded);
+            Assert.Equal("Missing Authorization Header", result.Failure.Message);
+
+        }
+
+        [Fact]
+        public async Task BadAuthorizationHeader()
+        {
+            var context = new DefaultHttpContext();
+            var authorizationHeader = new StringValues("Basc VGVzdc3RQYXNzd29yZA==");
+            context.Request.Headers.Add(HeaderNames.Authorization, authorizationHeader);
+
+            await _handler.InitializeAsync(
+                new AuthenticationScheme("Basic",
+                "Basic",
+                typeof(BasicAuthenticationHandler)),
+                context);
+
+            var result = await _handler.AuthenticateAsync();
+
+            Assert.False(result.Succeeded);
+            Assert.Equal("Invalid Authorization Header", result.Failure.Message);
+        }
+
+        [Fact]
+        public async Task ValidHeaderInvalidUser()
+        {
+            var context = new DefaultHttpContext();
+            var authorizationHeader = new StringValues($"Basic {EncodeAuthData("not-a-user", "123456789")}");
+            context.Request.Headers.Add(HeaderNames.Authorization, authorizationHeader);
+
+            await _handler.InitializeAsync(
+                new AuthenticationScheme("Basic",
+                "Basic",
+                typeof(BasicAuthenticationHandler)),
+                context);
+
+            var result = await _handler.AuthenticateAsync();
+
+            Assert.False(result.Succeeded);
+            Assert.Equal("Invalid Username or Password", result.Failure.Message);
+        }
+
+        [Fact]
+        public async Task ValidUser()
+        {
+            using (UserService userService = new UserService())
+            {
+                User user = await userService.CreateUser(new CreateUser { DisplayName = "handler-test-user" });
+
+                // plain text password wont have the dot
+                Assert.Single(user.Password.Split("."));
+
+                Assert.True(await userService.UserExists(user));
+
+                var context = new DefaultHttpContext();
+                var authorizationHeader = new StringValues($"Basic {EncodeAuthData(user._Id.ToString(), user.Password)}");
+                context.Request.Headers.Add(HeaderNames.Authorization, authorizationHeader);
+
+                await _handler.InitializeAsync(
+                    new AuthenticationScheme("Basic",
+                    "Basic",
+                    typeof(BasicAuthenticationHandler)),
+                    context);
+
+                var result = await _handler.AuthenticateAsync();
+
+                Assert.True(result.Succeeded);
+
+                await userService.RemoveUser(user);
+            }   
+        }
+    }
+}
